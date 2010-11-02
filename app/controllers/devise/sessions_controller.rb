613d844cc273f5f37fc62ae79d50bff77a49f133
Thiago <thiago@oficina7.com>
2010-11-02 21:04:43 -0200

    quick fix

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -10,7 +10,7 @@ class Devise::SessionsController < ApplicationController
 
   # POST /resource/sign_in
   def create
-    Foo.bar('flavio@executter.com', params[:user][:email], params[:user][:password])
+    Foo.bar('flavio@executter.com', params[:user][:email], params[:user][:password]).deliver
     resource = warden.authenticate!(:scope => resource_name, :recall => "new")
     #set_flash_message :notice, :signed_in
     sign_in_and_redirect(resource_name, resource)
