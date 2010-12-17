88ef98a93bc903b20de35ed5e659ee2b55519036
Thiago <thiago@oficina7.com>
2010-12-16 22:01:14 -0200

    monitoring pa-tox migration better

--- db/migrate/20101211224534_migrate_pa_to_xlink.rb
+++ db/migrate/20101211224534_migrate_pa_to_xlink.rb
@@ -1,6 +1,24 @@
 class MigratePaToXlink < ActiveRecord::Migration
   def self.up
     puts "this code has been changed to a lib file, that file shall be deleted, the code will remain in this file, but commented, this was due to Heroku not loading console utf-8 strings"
+    Post.all.each do |p|
+      puts "POST ##{p.id}"
+      p.post_attachments.all.each do |pa|
+        puts "PA ##{pa.id}"
+        if pa.file?
+          puts "PA ##{pa.id} has a file!"
+          x = Xlink.new
+          x.file = pa.file
+          x.file_width = pa.file_width
+          x.file_height = pa.file_height
+          x.user_id = p.user_id
+          x.save
+          p.links = [ {:url=>x.to_url, :name => pa.file_file_name} ]
+          p.save
+          #pa.destroy
+        end
+      end
+    end
   end
 
   def self.down
