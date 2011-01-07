9cb4e15c5d29d837e432db45f16099614e284166
Thiago <thiago@oficina7.com>
2011-01-07 04:46:17 -0200

    same, OMG I'm distracted today

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -8,7 +8,7 @@ class EventMailer < ActionMailer::Base
   #
   def followed(follower_relationship)
     @user_follower, @user_followed = follower_relationship.user1, follower_relationship.user2
-    subject = "#{@user_follower.username} agora esta seguindo você"
+    subject = "#{@user_follower.username} agora esta seguindo voce"
     
     mail :to => @user_followed.email, :subject => subject
   end
