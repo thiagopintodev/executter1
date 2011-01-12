158b193a6ddd5ead6851126a49b3b0e55d039d4f
Thiago <thiago@oficina7.com>
2011-01-11 22:48:44 -0200

    sending emails in packages seem to work now

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,15 +13,7 @@ class AdminController < ApplicationController
   end
 
   def do_emails
-    user_ids = DelayedMailFollowed.select("distinct user_id").limit(10).collect &:user_id
-    user_ids.each do |user_id|
-      @user = User.find(user_id)
-      dmf_list = DelayedMailFollowed.where(:user_id=>user_id).includes(:follower_user)
-      dmf_list
-      @followers_user = dmf_list.collect(&:follower_user)
-      EventMailer.followed @user, @followers_user
-      m.deliver
-    end
+    MyF.do_emails
     render :nothing => true
   end
 
--- lib/my_f.rb
+++ lib/my_f.rb
@@ -16,6 +16,22 @@ class MyF
     def production?
       ENV['BUCKET_NAME'] == 'production'
     end
+
+    def do_emails
+      user_ids = DelayedMailFollowed.select("distinct user_id").limit(10).collect &:user_id
+      puts "fetching #{user_ids.length} distinct emails to send --> #{Time.now}"
+      user_ids.each do |user_id|
+        user = User.find(user_id)
+        dmf_list = DelayedMailFollowed.where(:user_id=>user_id).includes(:follower_user)
+        followers_user = dmf_list.collect(&:follower_user)
+        ids = dmf_list.collect(&:id)
+        puts "sending #{ids.length} notifications to @#{user.email}  --> #{Time.now}"
+        m = EventMailer.followed user, followers_user
+        m.deliver
+        DelayedMailFollowed.delete(ids)
+      end
+      puts "done! sending #{user_ids.length} distinct emails  --> #{Time.now}"
+    end
     
   end
 end
