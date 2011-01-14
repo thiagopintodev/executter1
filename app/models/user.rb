c1c9aa8a4d18078e3e0fb88e06e17a7e8787c04e
Thiago <thiago@oficina7.com>
2011-01-14 04:37:20 -0200

    more user trace in admin panel, user full_name max 40 chars

--- app/models/user.rb
+++ app/models/user.rb
@@ -139,7 +139,7 @@ class User < ActiveRecord::Base
     :presence => true, :length => { :in => 2..16 },
     :uniqueness => {:case_sensitive => false}
   validates :full_name,
-    :presence => true, :length => { :minimum => 2 }
+    :presence => true, :length => { :in => 2..40 }
   validate :custom_validations
 
   def custom_validations
