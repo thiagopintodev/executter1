c90d3bc7da30c1f78af4bfb1b921e731a687746b
Thiago <thiago@oficina7.com>
2011-06-02 20:42:29 -0300

    user excepts

--- app/controllers/pure_controller.rb
+++ app/controllers/pure_controller.rb
@@ -1,6 +1,16 @@
 class PureController < ApplicationController
   def users
-    except = [:updated_at]
+    except = [
+      :updated_at, :admin, :authentication_token,:failed_attempts, :current_sign_in_ip, :current_sign_in_at,
+      :background_attachment_policy,:background_color, :background_image_content_type, :background_image_file_name,
+      :background_image_file_size,:background_image_updated_at,:background_position, :background_repeat_policy,
+      :birth_policy, :confirmation_sent_at, :confirmation_token, :confirmed_at, :remember_created_at,
+      :last_sign_in_at, :last_sign_in_ip, :locked_at, :reset_password_token, :remember_token,
+      :photo_id, :post_id, :posts_count, :relations_hash_count, :unlock_token, :sign_in_count      
+    ]
+
+    
+    
     @users = User.limit(100).offset(params[:skip])
     render :json => @users.to_json(:except=>except)
   end
