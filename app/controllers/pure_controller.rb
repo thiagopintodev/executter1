6207b0780ad450fc06b9afff7f41eec843d016c9
Thiago <thiago@oficina7.com>
2011-06-02 20:48:37 -0300

    user excepts

--- app/controllers/pure_controller.rb
+++ app/controllers/pure_controller.rb
@@ -11,24 +11,24 @@ class PureController < ApplicationController
 
     
     
-    @users = User.limit(100).offset(params[:skip])
+    @users = User.limit(100).offset(params[:skip]).order("id")
     render :json => @users.to_json(:except=>except)
   end
 
   def photos
-    @photos = Photo.limit(100).offset(params[:skip]).includes(:user)
+    @photos = Photo.limit(100).offset(params[:skip]).order("id").includes(:user)
     render :json => @photos.to_json(:only=>:created_at, :methods=>[:url, :user_username])
   end
 
   def posts
     except = [:updated_at, :subject_id, :usernames, :file_types]
-    @posts = Post.limit(100).offset(params[:skip])
+    @posts = Post.limit(100).offset(params[:skip]).order("id").includes(:user)
     render :json => @posts.to_json(:methods => [:user_username], :except=>except)
   end
 
   def relationships
     except = [:ignored_subjects, :user1_id, :user2_id, :created_at, :updated_at, :is_friend]
-    @relationships = Relationship.limit(100).offset(params[:skip])
+    @relationships = Relationship.limit(100).offset(params[:skip]).order("id").includes(:user1, :user2)
     render :json => @relationships.to_json(:methods => [:user1_username, :user2_username], :except=>except)
   end
 
