02947b98e19ecfd78bff8969ac50e636309d8b0c
Thiago <thiago@oficina7.com>
2011-06-03 17:03:32 -0300

    no more pf exporting

--- app/controllers/pure_controller.rb
+++ app/controllers/pure_controller.rb
@@ -27,7 +27,8 @@ class PureController < ApplicationController
   def posts
     except = [:updated_at, :subject_id, :usernames, :file_types, :links]
     @posts = Post.limit(1000).after(params[:last_post_id]).order("id").includes(:user)
-    render :json => @posts.to_json(:methods => [:user_username, :filename, :url], :except=>except)
+    render :json => @posts.to_json(:methods => [:user_username], :except=>except)
+    #, :filename, :url
   end
 
   def relationships
